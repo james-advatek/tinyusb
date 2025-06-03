2b995594326df7f059ee5abf9a46dc6f7a016ac3
James Sandison <james.s@advateklighting.com>
2025-06-03 17:19:02 +1000

    fix: remove unneeded RISAF and MPU config

--- hw/bsp/stm32n6/boards/stm32n657nucleo/board.h
+++ hw/bsp/stm32n6/boards/stm32n657nucleo/board.h
@@ -33,77 +33,75 @@
 #define BOARD_H_
 
 #ifdef __cplusplus
- extern "C" {
+extern "C" {
 #endif
 
-#include "tcpp0203.h"
 #include "stm32n657xx.h"
 #include "stm32n6xx_ll_exti.h"
 #include "stm32n6xx_ll_system.h"
+#include "tcpp0203.h"
 
-#define UART_DEV              USART1
-#define UART_CLK_EN           __HAL_RCC_USART1_CLK_ENABLE
+#define UART_DEV USART1
+#define UART_CLK_EN __HAL_RCC_USART1_CLK_ENABLE
+
+#define BOARD_TUD_RHPORT 1
 
 // VBUS Sense detection
-#define OTG_FS_VBUS_SENSE     1
-#define OTG_HS_VBUS_SENSE     1
+#define OTG_FS_VBUS_SENSE 1
+#define OTG_HS_VBUS_SENSE 1
 
-#define PINID_LED      0
-#define PINID_BUTTON   1
-#define PINID_UART_TX  2
-#define PINID_UART_RX  3
-#define PINID_TCPP0203_EN  4
+#define PINID_LED 0
+#define PINID_BUTTON 1
+#define PINID_UART_TX 2
+#define PINID_UART_RX 3
+#define PINID_TCPP0203_EN 4
 
 static board_pindef_t board_pindef[] = {
-  { // LED
-    .port = GPIOG,
-    .pin_init = { .Pin = GPIO_PIN_10, .Mode = GPIO_MODE_OUTPUT_PP, .Pull = GPIO_PULLDOWN, .Speed = GPIO_SPEED_FREQ_HIGH, .Alternate = 0 },
-    .active_state = 1
-  },
-  { // Button
-    .port = GPIOC,
-    .pin_init = { .Pin = GPIO_PIN_13, .Mode = GPIO_MODE_INPUT, .Pull = GPIO_PULLDOWN, .Speed = GPIO_SPEED_FREQ_HIGH, .Alternate = 0 },
-    .active_state = 1
-  },
-  { // UART TX
-    .port = GPIOE,
-    .pin_init = { .Pin = GPIO_PIN_5, .Mode = GPIO_MODE_AF_PP, .Pull = GPIO_NOPULL, .Speed = GPIO_SPEED_FREQ_LOW, .Alternate = GPIO_AF7_USART1 },
-    .active_state = 0
-  },
-  { // UART RX
-    .port = GPIOE,
-    .pin_init = { .Pin = GPIO_PIN_6, .Mode = GPIO_MODE_AF_PP, .Pull = GPIO_NOPULL, .Speed = GPIO_SPEED_FREQ_LOW, .Alternate = GPIO_AF7_USART1 },
-    .active_state = 0
-  },
-  { // VBUS input pin used for TCPP0203 EN
-    .port = GPIOA,
-    .pin_init = { .Pin = GPIO_PIN_7, .Mode = GPIO_MODE_OUTPUT_PP, .Pull = GPIO_PULLDOWN, .Speed = GPIO_SPEED_FREQ_HIGH, .Alternate = 0 },
-    .active_state = 0
-  },
-  { // I2C SCL for TCPP0203
-    .port = GPIOB,
-    .pin_init = { .Pin = GPIO_PIN_10, .Mode = GPIO_MODE_AF_OD, .Pull = GPIO_NOPULL, .Speed = GPIO_SPEED_FREQ_LOW, .Alternate = GPIO_AF4_I2C2 },
-  },
-  { // I2C SDA for TCPP0203
-    .port = GPIOB,
-    .pin_init = { .Pin = GPIO_PIN_11, .Mode = GPIO_MODE_AF_OD, .Pull = GPIO_NOPULL, .Speed = GPIO_SPEED_FREQ_LOW, .Alternate = GPIO_AF4_I2C2 },
-  },
-  { // INT for TCPP0203
-    .port = GPIOD,
-    .pin_init = { .Pin = GPIO_PIN_2, .Mode = GPIO_MODE_IT_FALLING, .Pull = GPIO_PULLUP, .Speed = GPIO_SPEED_FREQ_HIGH, .Alternate = 0 },
-  },
+    {// LED
+     .port = GPIOG,
+     .pin_init = {.Pin = GPIO_PIN_10, .Mode = GPIO_MODE_OUTPUT_PP, .Pull = GPIO_PULLDOWN, .Speed = GPIO_SPEED_FREQ_HIGH, .Alternate = 0},
+     .active_state = 1},
+    {// Button
+     .port = GPIOC,
+     .pin_init = {.Pin = GPIO_PIN_13, .Mode = GPIO_MODE_INPUT, .Pull = GPIO_PULLDOWN, .Speed = GPIO_SPEED_FREQ_HIGH, .Alternate = 0},
+     .active_state = 1},
+    {// UART TX
+     .port = GPIOE,
+     .pin_init = {.Pin = GPIO_PIN_5, .Mode = GPIO_MODE_AF_PP, .Pull = GPIO_NOPULL, .Speed = GPIO_SPEED_FREQ_LOW, .Alternate = GPIO_AF7_USART1},
+     .active_state = 0},
+    {// UART RX
+     .port = GPIOE,
+     .pin_init = {.Pin = GPIO_PIN_6, .Mode = GPIO_MODE_AF_PP, .Pull = GPIO_NOPULL, .Speed = GPIO_SPEED_FREQ_LOW, .Alternate = GPIO_AF7_USART1},
+     .active_state = 0},
+    {// VBUS input pin used for TCPP0203 EN
+     .port = GPIOA,
+     .pin_init = {.Pin = GPIO_PIN_7, .Mode = GPIO_MODE_OUTPUT_PP, .Pull = GPIO_PULLDOWN, .Speed = GPIO_SPEED_FREQ_HIGH, .Alternate = 0},
+     .active_state = 0},
+    {
+        // I2C SCL for TCPP0203
+        .port = GPIOB,
+        .pin_init = {.Pin = GPIO_PIN_10, .Mode = GPIO_MODE_AF_OD, .Pull = GPIO_NOPULL, .Speed = GPIO_SPEED_FREQ_LOW, .Alternate = GPIO_AF4_I2C2},
+    },
+    {
+        // I2C SDA for TCPP0203
+        .port = GPIOB,
+        .pin_init = {.Pin = GPIO_PIN_11, .Mode = GPIO_MODE_AF_OD, .Pull = GPIO_NOPULL, .Speed = GPIO_SPEED_FREQ_LOW, .Alternate = GPIO_AF4_I2C2},
+    },
+    {
+        // INT for TCPP0203
+        .port = GPIOD,
+        .pin_init = {.Pin = GPIO_PIN_2, .Mode = GPIO_MODE_IT_FALLING, .Pull = GPIO_PULLUP, .Speed = GPIO_SPEED_FREQ_HIGH, .Alternate = 0},
+    },
 };
 
 //--------------------------------------------------------------------+
 // RCC Clock
 //--------------------------------------------------------------------+
-void SystemClock_Config(void)
-{
+void SystemClock_Config(void) {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
   /* Configure the power domain */
-  if (HAL_PWREx_ConfigSupply(PWR_EXTERNAL_SOURCE_SUPPLY) != HAL_OK)
-  {
+  if (HAL_PWREx_ConfigSupply(PWR_EXTERNAL_SOURCE_SUPPLY) != HAL_OK) {
     Error_Handler();
   }
 
@@ -112,15 +110,13 @@ void SystemClock_Config(void)
   /* to ensure target clock can be set                 */
   HAL_RCC_GetClockConfig(&RCC_ClkInitStruct);
   if ((RCC_ClkInitStruct.CPUCLKSource == RCC_CPUCLKSOURCE_IC1) ||
-      (RCC_ClkInitStruct.SYSCLKSource == RCC_SYSCLKSOURCE_IC2_IC6_IC11))
-  {
+      (RCC_ClkInitStruct.SYSCLKSource == RCC_SYSCLKSOURCE_IC2_IC6_IC11)) {
     RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_CPUCLK | RCC_CLOCKTYPE_SYSCLK);
     RCC_ClkInitStruct.CPUCLKSource = RCC_CPUCLKSOURCE_HSI;
     RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct) != HAL_OK) {
+      Error_Handler();
+    }
   }
 
   /* HSE selected as source (stable clock on Level 0 samples */
@@ -133,7 +129,7 @@ void SystemClock_Config(void)
   /* PLL4 off                                                */
 
   /* Enable HSE && HSI */
-  RCC_OscInitStruct.OscillatorType =  RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSIState = RCC_HSI_OFF;
   RCC_OscInitStruct.HSIDiv = RCC_HSI_DIV1;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
@@ -141,14 +137,13 @@ void SystemClock_Config(void)
 
   RCC_OscInitStruct.PLL1.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL1.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL1.PLLM  = 3;
-  RCC_OscInitStruct.PLL1.PLLN  = 75;  /* PLL1 VCO = 48/3 * 75 = 1200MHz */
-  RCC_OscInitStruct.PLL1.PLLP1 = 1;   /* PLL output = PLL VCO frequency / (PLLP1 * PLLP2) */
-  RCC_OscInitStruct.PLL1.PLLP2 = 1;   /* PLL output = 1200 MHz */
+  RCC_OscInitStruct.PLL1.PLLM = 3;
+  RCC_OscInitStruct.PLL1.PLLN = 75; /* PLL1 VCO = 48/3 * 75 = 1200MHz */
+  RCC_OscInitStruct.PLL1.PLLP1 = 1; /* PLL output = PLL VCO frequency / (PLLP1 * PLLP2) */
+  RCC_OscInitStruct.PLL1.PLLP2 = 1; /* PLL output = 1200 MHz */
   RCC_OscInitStruct.PLL1.PLLFractional = 0;
 
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
     /* Initialization error */
     Error_Handler();
   }
@@ -159,8 +154,8 @@ void SystemClock_Config(void)
   /* Configure the HCLK clock divider */
   /* HCLK =  PLL1 SYSCLK/HCLK divider = 200 MHz */
   /* PCLKx = HCLK / PCLKx divider = 200 MHz */
-  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_CPUCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK  | \
-                                 RCC_CLOCKTYPE_PCLK1  | RCC_CLOCKTYPE_PCLK2 | RCC_CLOCKTYPE_PCLK4  | RCC_CLOCKTYPE_PCLK5);
+  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_CPUCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK |
+                                 RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2 | RCC_CLOCKTYPE_PCLK4 | RCC_CLOCKTYPE_PCLK5);
   RCC_ClkInitStruct.CPUCLKSource = RCC_CPUCLKSOURCE_IC1;
   RCC_ClkInitStruct.IC1Selection.ClockSelection = RCC_ICCLKSOURCE_PLL1;
   RCC_ClkInitStruct.IC1Selection.ClockDivider = 2;
@@ -176,8 +171,27 @@ void SystemClock_Config(void)
   RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV1;
   RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV1;
   RCC_ClkInitStruct.APB5CLKDivider = RCC_APB5_DIV1;
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct) != HAL_OK)
-  {
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct) != HAL_OK) {
+    /* Initialization Error */
+    Error_Handler();
+  }
+
+  /** Initializes the peripherals clock
+    */
+  RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = {0};
+  PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_USBOTGHS1;
+  PeriphClkInitStruct.UsbOtgHs1ClockSelection = RCC_USBPHY1REFCLKSOURCE_HSE_DIRECT;
+
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK) {
+    /* Initialization Error */
+    Error_Handler();
+  }
+
+  /** Set USB OTG HS PHY1 Reference Clock Source */
+  PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_USBPHY1;
+  PeriphClkInitStruct.UsbPhy1ClockSelection = RCC_USBPHY1REFCLKSOURCE_HSE_DIRECT;
+
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK) {
     /* Initialization Error */
     Error_Handler();
   }
@@ -187,22 +201,21 @@ void SystemClock_Config(void)
 // USB PD
 //--------------------------------------------------------------------+
 static I2C_HandleTypeDef i2c_handle = {
-  .Instance = I2C2,
-  .Init = {
-    .Timing = 0x20C0EDFF,
-    .OwnAddress1 = 0,
-    .AddressingMode = I2C_ADDRESSINGMODE_7BIT,
-    .DualAddressMode = I2C_DUALADDRESS_DISABLE,
-    .OwnAddress2 = 0,
-    .OwnAddress2Masks = I2C_OA2_NOMASK,
-    .GeneralCallMode = I2C_GENERALCALL_DISABLE,
-    .NoStretchMode = I2C_NOSTRETCH_DISABLE,
-  }
-};
-static TCPP0203_Object_t tcpp0203_obj = { 0 };
+    .Instance = I2C2,
+    .Init = {
+        .Timing = 0x20C0EDFF,
+        .OwnAddress1 = 0,
+        .AddressingMode = I2C_ADDRESSINGMODE_7BIT,
+        .DualAddressMode = I2C_DUALADDRESS_DISABLE,
+        .OwnAddress2 = 0,
+        .OwnAddress2Masks = I2C_OA2_NOMASK,
+        .GeneralCallMode = I2C_GENERALCALL_DISABLE,
+        .NoStretchMode = I2C_NOSTRETCH_DISABLE,
+    }};
+static TCPP0203_Object_t tcpp0203_obj = {0};
 
 int32_t board_tcpp0203_init(void) {
-  board_pindef_t* pindef = &board_pindef[PINID_TCPP0203_EN];
+  board_pindef_t *pindef = &board_pindef[PINID_TCPP0203_EN];
   HAL_GPIO_WritePin(pindef->port, pindef->pin_init.Pin, GPIO_PIN_SET);
 
   __HAL_RCC_I2C2_CLK_ENABLE();
@@ -223,7 +236,7 @@ int32_t board_tcpp0203_deinit(void) {
 }
 
 int32_t i2c_readreg(uint16_t DevAddr, uint16_t Reg, uint8_t *pData, uint16_t Length) {
-  TU_ASSERT (HAL_OK == HAL_I2C_Mem_Read(&i2c_handle, DevAddr, Reg, I2C_MEMADD_SIZE_8BIT, pData, Length, 10000));
+  TU_ASSERT(HAL_OK == HAL_I2C_Mem_Read(&i2c_handle, DevAddr, Reg, I2C_MEMADD_SIZE_8BIT, pData, Length, 10000));
   return 0;
 }
 
@@ -233,13 +246,13 @@ int32_t i2c_writereg(uint16_t DevAddr, uint16_t Reg, uint8_t *pData, uint16_t Le
 }
 
 static inline void board_init2(void) {
-  TCPP0203_IO_t            io_ctx;
+  TCPP0203_IO_t io_ctx;
 
-  io_ctx.Address     = TCPP0203_I2C_ADDRESS_X68;
-  io_ctx.Init        = board_tcpp0203_init;
-  io_ctx.DeInit      = board_tcpp0203_deinit;
-  io_ctx.ReadReg     = i2c_readreg;
-  io_ctx.WriteReg    = i2c_writereg;
+  io_ctx.Address = TCPP0203_I2C_ADDRESS_X68;
+  io_ctx.Init = board_tcpp0203_init;
+  io_ctx.DeInit = board_tcpp0203_deinit;
+  io_ctx.ReadReg = i2c_readreg;
+  io_ctx.WriteReg = i2c_writereg;
 
   TU_ASSERT(TCPP0203_RegisterBusIO(&tcpp0203_obj, &io_ctx) == TCPP0203_OK, );
 
@@ -256,15 +269,15 @@ void board_vbus_set(uint8_t rhport, bool state) {
 }
 
 void EXTI8_IRQHandler(void) {
-    __HAL_GPIO_EXTI_CLEAR_IT(GPIO_PIN_8);
-    if (tcpp0203_obj.IsInitialized) {
-      TU_ASSERT(TCPP0203_SetPowerMode(&tcpp0203_obj, TCPP0203_POWER_MODE_NORMAL) == TCPP0203_OK, );
-      TU_ASSERT(TCPP0203_SetGateDriverProvider(&tcpp0203_obj, TCPP0203_GD_PROVIDER_SWITCH_CLOSED) == TCPP0203_OK, );
-    }
+  __HAL_GPIO_EXTI_CLEAR_IT(GPIO_PIN_8);
+  if (tcpp0203_obj.IsInitialized) {
+    TU_ASSERT(TCPP0203_SetPowerMode(&tcpp0203_obj, TCPP0203_POWER_MODE_NORMAL) == TCPP0203_OK, );
+    TU_ASSERT(TCPP0203_SetGateDriverProvider(&tcpp0203_obj, TCPP0203_GD_PROVIDER_SWITCH_CLOSED) == TCPP0203_OK, );
+  }
 }
 
 #ifdef __cplusplus
- }
+}
 #endif
 
 #endif
--- hw/bsp/stm32n6/family.c
+++ hw/bsp/stm32n6/family.c
@@ -36,8 +36,6 @@
 
 TU_ATTR_UNUSED static void Error_Handler(void) { }
 
-static void MPU_Config(void);
-static void RISAF_Config(void);
 void HardFault_Handler(void);
 
 typedef struct {
@@ -129,14 +127,10 @@ void board_init(void) {
   HAL_PWREx_EnableVddIO4();
   HAL_PWREx_EnableVddIO5();
 
-  MPU_Config();
-
   HAL_Init();
 
   // Implemented in board.h
   SystemClock_Config();
-
-  RISAF_Config();
 
   // Enable All GPIOs clocks
   __HAL_RCC_GPIOA_CLK_ENABLE();
@@ -183,26 +177,62 @@ void board_init(void) {
 
 
   __HAL_RCC_USB1_OTG_HS_CLK_ENABLE();
-  PCD_HandleTypeDef hpcd_USB1_OTG_HS;
-  /* USER CODE BEGIN USB1_OTG_HS_Init 1 */
-  memset(&hpcd_USB1_OTG_HS, 0x0, sizeof(PCD_HandleTypeDef));
 
-  /* USER CODE END USB1_OTG_HS_Init 1 */
-  hpcd_USB1_OTG_HS.Instance = USB1_OTG_HS;
-  hpcd_USB1_OTG_HS.Init.dev_endpoints = 9;
-  hpcd_USB1_OTG_HS.Init.speed = PCD_SPEED_HIGH;
-  hpcd_USB1_OTG_HS.Init.dma_enable = DISABLE;
-  hpcd_USB1_OTG_HS.Init.phy_itface = USB_OTG_HS_EMBEDDED_PHY;
-  hpcd_USB1_OTG_HS.Init.Sof_enable = DISABLE;
-  hpcd_USB1_OTG_HS.Init.low_power_enable = DISABLE;
-  hpcd_USB1_OTG_HS.Init.lpm_enable = DISABLE;
-  hpcd_USB1_OTG_HS.Init.vbus_sensing_enable = DISABLE;
-  hpcd_USB1_OTG_HS.Init.use_dedicated_ep1 = DISABLE;
-  hpcd_USB1_OTG_HS.Init.use_external_vbus = DISABLE;
-  if (HAL_PCD_Init(&hpcd_USB1_OTG_HS) != HAL_OK)
-  {
-    HardFault_Handler();
-  }
+  __HAL_RCC_PWR_CLK_ENABLE();
+  HAL_PWREx_EnableVddUSBVMEN();
+  while(__HAL_PWR_GET_FLAG(PWR_FLAG_USB33RDY));
+  HAL_PWREx_EnableVddUSB();
+
+  LL_AHB5_GRP1_ForceReset(0x00800000);
+  __HAL_RCC_USB1_OTG_HS_FORCE_RESET();
+  __HAL_RCC_USB1_OTG_HS_PHY_FORCE_RESET();
+
+  LL_RCC_HSE_SelectHSEDiv2AsDiv2Clock();
+  LL_AHB5_GRP1_ReleaseReset(0x00800000);
+
+  /* Peripheral clock enable */
+  __HAL_RCC_USB1_OTG_HS_CLK_ENABLE();
+
+  /* Required few clock cycles before accessing USB PHY Controller Registers */
+  HAL_Delay(1);
+
+  USB1_HS_PHYC->USBPHYC_CR &= ~(0x7 << 0x4);
+
+  USB1_HS_PHYC->USBPHYC_CR |= (0x1 << 16) |
+                              (0x2 << 4)  |
+                              (0x1 << 2)  |
+                                0x1U;
+
+  __HAL_RCC_USB1_OTG_HS_PHY_RELEASE_RESET();
+
+  /* Required few clock cycles before Releasing Reset */
+  HAL_Delay(1);
+
+  __HAL_RCC_USB1_OTG_HS_RELEASE_RESET();
+
+  /* Peripheral PHY clock enable */
+  __HAL_RCC_USB1_OTG_HS_PHY_CLK_ENABLE();
+
+  // PCD_HandleTypeDef hpcd_USB1_OTG_HS;
+  // /* USER CODE BEGIN USB1_OTG_HS_Init 1 */
+  // memset(&hpcd_USB1_OTG_HS, 0x0, sizeof(PCD_HandleTypeDef));
+
+  // /* USER CODE END USB1_OTG_HS_Init 1 */
+  // hpcd_USB1_OTG_HS.Instance = USB1_OTG_HS;
+  // hpcd_USB1_OTG_HS.Init.dev_endpoints = 9;
+  // hpcd_USB1_OTG_HS.Init.speed = PCD_SPEED_HIGH;
+  // hpcd_USB1_OTG_HS.Init.dma_enable = DISABLE;
+  // hpcd_USB1_OTG_HS.Init.phy_itface = USB_OTG_HS_EMBEDDED_PHY;
+  // hpcd_USB1_OTG_HS.Init.Sof_enable = DISABLE;
+  // hpcd_USB1_OTG_HS.Init.low_power_enable = DISABLE;
+  // hpcd_USB1_OTG_HS.Init.lpm_enable = DISABLE;
+  // hpcd_USB1_OTG_HS.Init.vbus_sensing_enable = DISABLE;
+  // hpcd_USB1_OTG_HS.Init.use_dedicated_ep1 = DISABLE;
+  // hpcd_USB1_OTG_HS.Init.use_external_vbus = DISABLE;
+  // if (HAL_PCD_Init(&hpcd_USB1_OTG_HS) != HAL_OK)
+  // {
+  //   HardFault_Handler();
+  // }
 
   board_init2();
 
@@ -277,148 +307,6 @@ uint32_t board_millis(void) {
 }
 
 #endif
-
-/* MPU Configuration */
-static void MPU_Config(void)
-{
-  MPU_Region_InitTypeDef default_config = {0};
-  MPU_Attributes_InitTypeDef attr_config = {0};
-  uint32_t primask_bit = __get_PRIMASK();
-  __disable_irq();
-
-  /* disable the MPU */
-  HAL_MPU_Disable();
-
-  /* create an attribute configuration for the MPU */
-  attr_config.Attributes = INNER_OUTER(MPU_NOT_CACHEABLE);
-  attr_config.Number = MPU_ATTRIBUTES_NUMBER0;
-
-  HAL_MPU_ConfigMemoryAttributes(&attr_config);
-
-  /*Normal memory type, code execution allowed */
-  default_config.Enable = MPU_REGION_ENABLE;
-  default_config.Number = MPU_REGION_NUMBER0;
-  default_config.BaseAddress = 0x341FA600;
-  default_config.LimitAddress =  0x341FFFFF;
-  default_config.DisableExec = MPU_INSTRUCTION_ACCESS_ENABLE;
-  default_config.AccessPermission = MPU_REGION_ALL_RW;
-  default_config.IsShareable = MPU_ACCESS_NOT_SHAREABLE;
-  default_config.AttributesIndex = MPU_ATTRIBUTES_NUMBER0;
-  HAL_MPU_ConfigRegion(&default_config);
-
-  /* enable the MPU */
-  HAL_MPU_Enable(MPU_PRIVILEGED_DEFAULT);
-
-  /* Exit critical section to lock the system and avoid any issue around MPU mechanisme */
-  __set_PRIMASK(primask_bit);
-}
-
-
-/**
-* @brief  RISAF Configuration.
-* @retval None
-*/
-static void RISAF_Config(void)
-{
-  RIMC_MasterConfig_t RIMC_master = {0};
-
-  __HAL_RCC_RIFSC_CLK_ENABLE();
-
-  RIMC_master.MasterCID = RIF_CID_1;
-  RIMC_master.SecPriv = RIF_ATTRIBUTE_SEC | RIF_ATTRIBUTE_PRIV;
-
-  HAL_RIF_RIMC_ConfigMasterAttributes(RIF_MASTER_INDEX_OTG1, &RIMC_master);
-  HAL_RIF_RIMC_ConfigMasterAttributes(RIF_MASTER_INDEX_OTG2, &RIMC_master);
-  HAL_RIF_RISC_SetSlaveSecureAttributes(RIF_RISC_PERIPH_INDEX_OTG1HS, RIF_ATTRIBUTE_SEC | RIF_ATTRIBUTE_PRIV);
-  HAL_RIF_RISC_SetSlaveSecureAttributes(RIF_RISC_PERIPH_INDEX_OTG2HS, RIF_ATTRIBUTE_SEC | RIF_ATTRIBUTE_PRIV);
-
-  HAL_RIF_RISC_SetSlaveSecureAttributes(RIF_RISC_PERIPH_INDEX_ADC12, RIF_ATTRIBUTE_SEC | RIF_ATTRIBUTE_PRIV);
-
-}
-
-
-/**
-* @brief PCD MSP Initialization
-* This function configures the hardware resources used in this example
-* @param hhcd: PCD handle pointer
-* @retval None
-*/
-void HAL_PCD_MspInit(PCD_HandleTypeDef* pcdHandle)
-{
-  if (pcdHandle->Instance==USB1_OTG_HS)
-  {
-    /* USER CODE BEGIN USB_OTG_HS_MspInit 0 */
-
-    /* USER CODE END USB_OTG_HS_MspInit 0 */
-    /* Enable VDDUSB */
-    __HAL_RCC_PWR_CLK_ENABLE();
-    HAL_PWREx_EnableVddUSBVMEN();
-    while(__HAL_PWR_GET_FLAG(PWR_FLAG_USB33RDY));
-    HAL_PWREx_EnableVddUSB();
-
-    /** Initializes the peripherals clock
-    */
-    RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = {0};
-    PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_USBOTGHS1;
-    PeriphClkInitStruct.UsbOtgHs1ClockSelection = RCC_USBPHY1REFCLKSOURCE_HSE_DIRECT;
-
-    if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
-    {
-      /* Initialization Error */
-      Error_Handler();
-    }
-
-    /** Set USB OTG HS PHY1 Reference Clock Source */
-    PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_USBPHY1;
-    PeriphClkInitStruct.UsbPhy1ClockSelection = RCC_USBPHY1REFCLKSOURCE_HSE_DIRECT;
-
-    if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
-    {
-      /* Initialization Error */
-      Error_Handler();
-    }
-
-    __HAL_RCC_GPIOA_CLK_ENABLE();
-
-    LL_AHB5_GRP1_ForceReset(0x00800000);
-    __HAL_RCC_USB1_OTG_HS_FORCE_RESET();
-    __HAL_RCC_USB1_OTG_HS_PHY_FORCE_RESET();
-
-    LL_RCC_HSE_SelectHSEDiv2AsDiv2Clock();
-    LL_AHB5_GRP1_ReleaseReset(0x00800000);
-
-    /* Peripheral clock enable */
-    __HAL_RCC_USB1_OTG_HS_CLK_ENABLE();
-
-    /* Required few clock cycles before accessing USB PHY Controller Registers */
-    HAL_Delay(1);
-
-    USB1_HS_PHYC->USBPHYC_CR &= ~(0x7 << 0x4);
-
-    USB1_HS_PHYC->USBPHYC_CR |= (0x1 << 16) |
-                                (0x2 << 4)  |
-                                (0x1 << 2)  |
-                                 0x1U;
-
-    __HAL_RCC_USB1_OTG_HS_PHY_RELEASE_RESET();
-
-    /* Required few clock cycles before Releasing Reset */
-    HAL_Delay(1);
-
-    __HAL_RCC_USB1_OTG_HS_RELEASE_RESET();
-
-    /* Peripheral PHY clock enable */
-    __HAL_RCC_USB1_OTG_HS_PHY_CLK_ENABLE();
-
-    /* USB_OTG_HS interrupt Init */
-    HAL_NVIC_SetPriority(USB1_OTG_HS_IRQn, 7, 0);
-    HAL_NVIC_EnableIRQ(USB1_OTG_HS_IRQn);
-
-    /* USER CODE BEGIN USB_OTG_HS_MspInit 1 */
-
-    /* USER CODE END USB_OTG_HS_MspInit 1 */
-  }
-}
 
 void HardFault_Handler(void) {
   __asm("BKPT #0\n");
